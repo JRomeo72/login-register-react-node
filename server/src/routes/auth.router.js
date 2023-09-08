ce7413dbb7d9da5c1fa9d86c15099ddab1c54df0
Romeo Hernandez <romeo.hernandez@Mac-mini-de-Estudio.local>
2023-09-08 08:34:05 -0600

    Commit Sept 8 2023 Bla

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authCtr from '../controllers/auth.controller.js';
+import { registerSchema, loginSchema } from "../validators/auth.validator.schema.js";
+import authValidator from "../middlewares/authValidator.js";
 
 let router = Router();
 
-router.post('/register', authCtr.register);
-router.post('/login', authCtr.login);
+router.post('/register', authValidator(registerSchema), authCtr.register);
+router.post('/login', authValidator(loginSchema), authCtr.login);
 router.post('/logout', authCtr.logout)
 
 export default router
